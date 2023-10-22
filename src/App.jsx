de2402eb07dd0f4b53561a94eec123288d4e29dc
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-10-22 19:31:48 -0400

    add nextui for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
+// 1. import `NextUIProvider` component
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+	const [number, setNumber] = useState('');
+	const [result, setResult] = useState(null);
+	const [percentage, setPercentage] = useState(null);
+
 	const calculatePercentage = (value) => {
+		let percentage = 0;
 		if (value < 2000) {
-			return value * 0.05;
+			percentage = 5;
 		} else if (value >= 2000 && value <= 4000) {
-			return value * 0.1;
+			percentage = 10;
 		} else if (value > 4000) {
-			return value * 0.1;
+			percentage = 15;
 		}
-		return 0;
+		setPercentage(percentage);
+		return (value * percentage) / 100;
 	};
 
-	const result = calculatePercentage(2500);
-	console.log(result); // This will log 250, which is 10% of 2500
+	const handleChange = (event) => {
+		const inputValue = event.target.value;
+		setNumber(inputValue);
+		if (inputValue) {
+			const calculatedResult = calculatePercentage(Number(inputValue));
+			setResult(calculatedResult);
+		} else {
+			setResult(null);
+			setPercentage(null);
+		}
+	};
 
 	return (
-		<>
-			<div className="card">{result}</div>
-		</>
+		<div className="App">
+			<div>{percentage !== null && <span>Percentage: {percentage}%</span>}</div>
+			<header className="App-header">
+				<input
+					type="number"
+					value={number}
+					onChange={handleChange}
+					placeholder="Enter a number"
+				/>
+				<div>{result !== null && <span>Result: {result.toFixed(2)}</span>}</div>
+			</header>
+		</div>
 	);
 }
 
